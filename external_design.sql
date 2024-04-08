b6e8b507f77a4d9d5704f767a88705b3f7de7859
Manas Khatore <94086185+Manas-Khatore@users.noreply.github.com>
2024-04-08 16:55:34 +0200

    starting second view

--- external_design.sql
+++ external_design.sql
@@ -1,4 +1,7 @@
-/* use USER function provided by SQL */
+/* use USER function provided by SQL, is it supposed to have a different value? */
 
 CREATE VIEW my_purchases AS 
 SELECT * FROM ORDERS_CLIENTS WHERE ORDERS_CLIENTS.username = (SELECT USER FROM DUAL);
+
+CREATE VIEW my_profile AS 
+SELECT 
