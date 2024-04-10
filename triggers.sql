287353c5ca8476758db1eb126d4417ea089aa6fd
Diego Gomez <104745376+DiegoGomez1@users.noreply.github.com>
2024-04-10 13:20:08 +0200

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,1 +1,18 @@
-
+CREATE OR REPLACE TRIGGER attribute_endorsed
+BEFORE INSERT OR UPDATE ON Posts
+FOR EACH ROW
+BEGIN
+    IF : NEW.product IN (
+          SELECT product
+          FROM References
+          WHERE barCode IN (
+              SELECT barCode
+              FROM Supply_Lines
+              WHERE taxID = :NEW.username
+          )
+      ) THEN
+          :NEW.endorsed := SYSDATE; 
+      ELSE 
+          :NEW.endorsed := null;
+      END IF;
+END;
